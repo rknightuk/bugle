340c2994eba92e5d8530900f4d364916f4b397ed
Robb Knight <desk@rknight.me>
2023-07-05 19:43:30 +0100

    Add 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Page Not Found')
+
+@section('content')
+
+    <p>Page not found :(</p>
+
+@endsection
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
+        basicSsl(),
         laravel({
             input: [
                 'resources/css/app.css',
